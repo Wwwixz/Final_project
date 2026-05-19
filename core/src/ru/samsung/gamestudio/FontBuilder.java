a47c08f463031c401c340977737a659793591bf3
Wwwixz <Wwwixz@users.noreply.github.com>
2026-05-19 22:02:07 +0300

    Добавлена поддержка кириллицы в генератор шрифтов

--- core/src/ru/samsung/gamestudio/FontBuilder.java
+++ core/src/ru/samsung/gamestudio/FontBuilder.java
@@ -7,9 +7,13 @@ import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class FontBuilder {
 
+    public static final String FONT_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=<>?:;{}[]|,./\\'\" " +
+            "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
     public static BitmapFont generate(int size, Color color, String fontPath) {
         FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal(fontPath));
         FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        parameter.characters = FONT_CHARS;
         parameter.size = size;
         parameter.color = color;
         BitmapFont font = generator.generateFont(parameter);
